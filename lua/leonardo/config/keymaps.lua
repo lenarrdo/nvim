507ce77a5616af1f60a71b6de063e3442d853a1c
Leonardo <leonardo.miani23@gmail.com>
2023-08-02 13:48:47 +0200

    <leader>+p to paste without overwriting register

--- lua/leonardo/config/keymaps.lua
+++ lua/leonardo/config/keymaps.lua
@@ -13,6 +13,9 @@ map("i", "<C-H>", "<C-W>", { noremap = true }) -- Linux
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Copia senza modificare il registro
+map("x", "<leader>p", "\"_dP")
+
 -- CTRL+D, CTRL+U, G cursore sempre centrato
 map("n", "<C-d>", "<C-d>zz", { noremap = true })
 map("n", "<C-u>", "<C-u>zz", { noremap = true })
