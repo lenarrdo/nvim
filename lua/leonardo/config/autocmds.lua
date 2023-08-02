dfad9833d0480804b433b0e0ebb2a8c167e80347
Leonardo <leonardo.miani23@gmail.com>
2023-08-02 13:58:16 +0200

    added highlight on yank

--- lua/leonardo/config/autocmds.lua
+++ lua/leonardo/config/autocmds.lua
@@ -1,6 +1,13 @@
+-- Disable new line comment
 vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
     vim.opt.formatoptions:remove({ "c", "r", "o" })
   end,
-  desc = "Disable New Line Comment",
+})
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
