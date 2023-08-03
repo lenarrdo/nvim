1166dd92901abd25789f49b291166901537693da
Leonardo <leonardo.miani23@gmail.com>
2023-08-03 13:42:18 +0200

    now you can cycle through snippets with CTRL+J/CTRL+K

--- lua/leonardo/plugins/completions.lua
+++ lua/leonardo/plugins/completions.lua
@@ -53,11 +53,21 @@ local cmp = require("cmp")
 
 cmp.setup ({
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+		["<C-j>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end, {"i", "s"}),
+		["<C-k>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end, {"i", "s"}),
   }),
   sources = cmp.config.sources({
     { name = "luasnip" },
@@ -67,4 +77,3 @@ cmp.setup ({
     { name = "buffer" },
   })
 })
-
