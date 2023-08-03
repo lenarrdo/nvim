c416d5917e92945ee572b03123710eb961fff257
Leonardo <leonardo.miani23@gmail.com>
2023-08-03 21:11:23 +0200

    super powerful <Tab>

--- lua/leonardo/plugins/completions.lua
+++ lua/leonardo/plugins/completions.lua
@@ -25,20 +25,11 @@ function()
 end,
 {silent = true})
 
--- Cycle backwards with Shift + Tab
-vim.keymap.set({ "i", "s" }, "<S-Tab>",
+-- Cycle backwards with CTRL + H
+vim.keymap.set({ "i", "s" }, "<C-h>",
 function()
   if luasnip.jumpable(-1) then
     luasnip.jump(-1)
-  end
-end,
-{silent = true})
-
--- Change option with CTRL + L
-vim.keymap.set({ "i", "s" }, "<c-l>",
-function()
-  if luasnip.choice_active() then
-    luasnip.change_choice(1)
   end
 end,
 {silent = true})
@@ -53,21 +44,33 @@ local cmp = require("cmp")
 
 cmp.setup ({
   mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-		["<C-j>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			else
-				fallback()
-			end
-		end, {"i", "s"}),
-		["<C-k>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			else
-				fallback()
-			end
-		end, {"i", "s"}),
+    --[[ In questo modo se ho scritto qualcosa come richiesto da luaSnip, allora
+    viene espanso quello. Altrimenti pesco la prima opzione dal menù. ]]
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() and not luasnip.expand_or_jumpable() then
+        local entry = cmp.get_selected_entry()
+        if not entry then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          cmp.confirm()
+        end
+      else
+        fallback()
+      end
+    end, {"i","s","c",}),
+    ["<C-j>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, {"i", "s"}),
+    ["<C-k>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, {"i", "s"}),
   }),
   sources = cmp.config.sources({
     { name = "luasnip" },
