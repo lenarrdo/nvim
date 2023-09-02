190383194817aec0c63d95de686cbfa0538b2382
Leonardo <leonardo.miani23@gmail.com>
2023-09-02 13:08:18 +0200

    lua lsp config

--- lua/leonardo/plugins/lsp.lua
+++ lua/leonardo/plugins/lsp.lua
@@ -6,8 +6,16 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+-- Configure lua language server for neovim
+require("lspconfig").lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim", "fmta", "i", "s", "f" }
+      }
+    }
+  }
+}
 
 lsp.skip_server_setup({'jdtls'})
 
