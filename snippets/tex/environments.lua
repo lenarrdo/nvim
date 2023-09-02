07bd303c33f3b97eb47cdfddba96db24606a1b46
Leonardo <leonardo.miani23@gmail.com>
2023-09-02 11:04:43 +0200

    auto environments

--- snippets/tex/environments.lua
+++ snippets/tex/environments.lua
@@ -1,74 +1,166 @@
+local line_begin = require("luasnip.extras.expand_conditions").line_begin
+local NIMandLB = function() -- Not In Mathzone and Line Begin
+  return vim.fn['vimtex#syntax#in_mathzone']() == 0 and line_begin
+end
+
 return {
-
-	------------------
-	-- ENVIRONMENTS --
-	------------------
-
-	s(
-		{ trig = "env", dscr = "Per creare un generico environment" },
-		fmta(
+  s({trig = "env", snippetType = "autosnippet"},
+    fmta(
 			[[
        \begin{<>}
          <>
        \end{<>}
-     ]],
+      ]],
+      {
+        i(1),
+        i(0),
+        rep(1),
+      }
+    ),
+    {condition = line_begin}
+  ),
+	s({ trig = "fig", snippetType = "autosnippet" },
+		fmta(
+			[[
+        \begin{figure}[H]
+          \centering
+          \includegraphics[width=0.5\textwidth]{./figures/<>}
+          \caption{<>}
+          \label{<>}
+        \end{figure}
+        \noindent <>
+      ]],
 			{
-				i(1),
-				i(2),
-				rep(1),
+				i(1, "file.jpeg"),
+        i(2),
+        i(3),
+        i(0),
 			}
 		)
 	),
-	s(
-		{ trig = "center", dscr = "Environment testo centrato" },
-		fmta(
+  s({trig = "cent", snippetType = "autosnippet"},
+    fmta(
 			[[
        \begin{center}
          <>
        \end{center}
-     ]],
-			{ i(1) }
-		)
-	),
-	s(
-		{ trig = "chap", dscr = "Nuovo capitolo" },
-		fmt("\\chapter{<>}", {
-			i(1),
-		}, { delimiters = "<>" })
-	),
-	s(
-		{ trig = "sec", dscr = "Nuova sezione" },
-		fmt("\\section{<>}", {
-			i(1),
-		}, { delimiters = "<>" })
-	),
-	s(
-		{ trig = "ssec", dscr = "Nuova subsection" },
-		fmt("\\subsection{<>}", {
-			i(1),
-		}, { delimiters = "<>" })
-	),
-	s(
-		{ trig = "sssec", dscr = "Nuova subsubsection" },
-		fmt("\\subsubsection{<>}", {
-			i(1),
-		}, { delimiters = "<>" })
-	),
-	s(
-		{ trig = "par", dscr = "Nuovo paragrafo" },
-		fmt("\\paragraph{<>}", {
-			i(1),
-		}, { delimiters = "<>" })
-	),
-	s(
-		{ trig = "mini", dscr = "Environment minipage" },
-		fmta(
+       <>
+      ]],
+      {
+        i(1),
+        i(0),
+      }
+    ),
+    {condition = line_begin}
+  ),
+  s({trig = ",e", snippetType = "autosnippet"},
+    fmta(
+    [[
+      \begin{enumerate}
+        \item <>
+      \end{enumerate}
+      <>
+    ]],
+    {
+      i(1),
+      i(0),
+    }
+    ),
+    {condition = NIMandLB}
+  ),
+  s({trig = ",i", snippetType = "autosnippet"},
+    fmta(
+    [[
+      \begin{itemize}
+        \item <>
+      \end{itemize}
+      <>
+    ]],
+    {
+      i(1),
+      i(0),
+    }
+    ),
+    {condition = NIMandLB}
+  ),
+  s({trig = "mini", snippetType = "autosnippet"},
+    fmta(
 			[[
        \begin{minipage}{0.5\textwidth}
          <>
        \end{minipage}
-     ]],
-			{ i(1) }
-		)
-	),
+       <>
+      ]],
+      {
+        i(1),
+        i(0),
+      }
+    ),
+    {condition = line_begin}
+  ),
+  s({trig = "pmat", snippetType = "autosnippet"},
+    fmta(
+			[[
+        \begin{pmatrix}
+          <>
+        \end{pmatrix}
+       <>
+      ]],
+      {
+        i(1),
+        i(0),
+      }
+    ),
+    {condition = line_begin}
+  ),
+  s({trig = "equ", snippetType = "autosnippet"},
+    fmta(
+			[[
+        \begin{equation}
+          <>
+        \end{equation}
+       <>
+      ]],
+      {
+        i(1),
+        i(0),
+      }
+    ),
+    {condition = line_begin}
+  ),
+  s({trig = "eqsp", snippetType = "autosnippet"},
+    fmta(
+			[[
+      \begin{equation*}
+        \begin{split}
+          <>
+        \end{split}
+      \end{equation*}
+       <>
+      ]],
+      {
+        i(1),
+        i(0),
+      }
+    ),
+    {condition = line_begin}
+  ),
+  s({trig = "cases", snippetType = "autosnippet"},
+    fmta(
+			[[
+       \begin{cases}
+        <> & \text{se } <> \\
+        <>
+       \end{cases}
+       <>
+      ]],
+      {
+        i(1),
+        i(2),
+        i(3),
+        i(0),
+      }
+    ),
+    {condition = line_begin}
+  ),
 }
