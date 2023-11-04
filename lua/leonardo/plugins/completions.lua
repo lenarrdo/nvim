611a10f5ed082709b2fb5bd584efeb320336ad10
Leonardo <leonardo.miani23@gmail.com>
2023-11-04 11:48:21 +0100

    smaller config and tab works as intended in command mode

--- lua/leonardo/plugins/completions.lua
+++ lua/leonardo/plugins/completions.lua
@@ -1,64 +1,35 @@
---[[
-
-LUASNIP
-
--- ]]
 local luasnip = require("luasnip")
+local cmp = require("cmp")
 
-luasnip.config.set_config({
-
-  -- Enable autotriggered snippets
+luasnip.config.setup({
   enable_autosnippets = true,
-
-  -- Load custom snippets
   require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets/" })
 })
 
--- Expand and cycle through a snippet with Tab.
-vim.keymap.set({ "i", "s" }, "<Tab>",
-function()
-  if luasnip.expand_or_jumpable() then
-    luasnip.expand_or_jump()
-  else
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
-  end
-end,
-{silent = true})
-
--- Cycle backwards with Shift + Tab
-vim.keymap.set({ "i", "s" }, "<S-Tab>",
-function()
-  if luasnip.jumpable(-1) then
-    luasnip.jump(-1)
-  end
-end,
-{silent = true})
-
---[[
-
-NVIM-CMP
-
--- ]]
-
-local cmp = require("cmp")
-
 cmp.setup ({
   mapping = cmp.mapping.preset.insert({
-    --[[ In questo modo se ho scritto qualcosa come richiesto da luaSnip, allora
-    viene espanso quello. Altrimenti pesco la prima opzione dal menù. ]]
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() and not luasnip.expand_or_jumpable() then
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif cmp.visible() then
         local entry = cmp.get_selected_entry()
-        if not entry then
-          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        if entry then
           cmp.confirm()
         else
+          cmp.select_next_item()
           cmp.confirm()
         end
       else
         fallback()
       end
-    end, {"i","s","c",}),
+    end, {"i","s"}),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {"i", "s"}),
     ["<C-j>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
