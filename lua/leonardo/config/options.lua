1ef3908cdf0f62074523c488db850d37e297d562
Leonardo <leonardo.miani23@gmail.com>
2023-11-17 18:48:22 +0100

    cleaner config

--- lua/leonardo/config/options.lua
+++ lua/leonardo/config/options.lua
@@ -1,53 +1,42 @@
-local o = vim.opt
-local g = vim.g
+local options = {
+  expandtab = true,
+  number = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  clipboard = "unnamedplus",
+  colorcolumn = "80",
+  confirm = true,
+  ignorecase = true,
+  list = true,
+  relativenumber = true,
+  scrolloff = 8,
+  showmode = false,
+  sidescrolloff = 8,
+  signcolumn = "yes",
+  smartcase = true,
+  smartindent = true,
+  spell = true,
+  spelllang = "it,en",
+  splitbelow = true,
+  splitright = true,
+  termguicolors = true,
+  timeout = true,
+  timeoutlen = 500,
+  undofile = true,
+  undolevels = 10000,
+  updatetime = 200,
+  wrap = false,
+}
 
-g.mapleader = " "
-g.maplocalleader = "\\"
-g.loaded_netrw = 1
-g.loaded_netrwPlugin = 1
-g.vimtex_view_method = "zathura"
+local global = {
+  mapleader = " ",
+  vimtex_view_method = "zathura",
+}
 
-o.autowrite = true -- Enable auto write
-o.clipboard = "unnamedplus" -- Sync with system clipboard
-o.colorcolumn = "100" -- Vertical ruler at column 80
-o.completeopt = "menu,menuone,noselect"
-o.conceallevel = 0 -- Hide * markup for bold and italic
-o.confirm = true -- Confirm to save changes before exiting modified buffer
-o.cursorline = true -- Enable highlighting of the current line
-o.expandtab = true -- Use spaces instead of tabs
-o.formatoptions = "jcroqlnt" -- tcqj
-o.grepformat = "%f:%l:%c:%m"
-o.grepprg = "rg --vimgrep"
-o.ignorecase = true -- Ignore case
-o.inccommand = "nosplit" -- preview incremental substitute
-o.laststatus = 0
-o.list = true -- Show some invisible characters (tabs...
-o.mouse = "a" -- Enable mouse mode
-o.number = true -- Print line number
-o.pumblend = 10 -- Popup blend
-o.pumheight = 10 -- Maximum number of entries in a popup
-o.relativenumber = true -- Relative line numbers
-o.scrolloff = 8 -- Lines of context
-o.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
-o.shiftround = true -- Round indent
-o.shiftwidth = 2 -- Size of an indent
-o.shortmess:append({ W = true, I = true, c = true })
-o.showmode = false -- Dont show mode since we have a statusline
-o.sidescrolloff = 8 -- Columns of context
-o.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-o.smartcase = true -- Don't ignore case with capitals
-o.smartindent = true -- Insert indents automatically
-o.spell = true
-o.spelllang = "it,en"
-o.splitbelow = true -- Put new windows below current
-o.splitright = true -- Put new windows right of current
-o.tabstop = 2 -- Number of spaces tabs count for
-o.termguicolors = true -- True color support
-o.timeout = true
-o.timeoutlen = 300
-o.undofile = true
-o.undolevels = 10000
-o.updatetime = 200 -- Save swap file and trigger CursorHold
-o.wildmode = "longest:full,full" -- Command-line completion mode
-o.winminwidth = 5 -- Minimum window width
-o.wrap = false -- Disable line wrap
+for name, value in pairs(options) do
+  vim.opt[name] = value
+end
+
+for name, value in pairs(global) do
+  vim.g[name] = value
+end
