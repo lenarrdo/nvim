f0a70e7b97e55d5f2a108a9060aa6f5a6aa747fc
leonardo <leonardo.miani23@gmail.com>
2023-12-09 11:32:44 +0100

    keymaps update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,10 @@
 local map = vim.keymap.set
 
 -- Better up/down
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ "n", "x" }, "j", "gj", { noremap = true, silent = true })
+map({ "n", "x" }, "k", "gk", { noremap = true, silent = true })
+map({ "n", "x" }, "gj", "j", { noremap = true, silent = true })
+map({ "n", "x" }, "gk", "k", { noremap = true, silent = true })
 
 -- Indentazione fiera
 map("v", "J", ":m '>+1<CR>gv=gv")
@@ -16,25 +18,19 @@ map("i", "<C-H>", "<C-W>", { noremap = true })
 -- Incolla senza modificare il registro
 map("x", "<leader>p", "\"_dP")
 
--- File explorer
-map("n", "<leader>pv", "<cmd>:Ex<cr>")
+-- File explorer (oil.nvim)
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- CTRL+D, CTRL+U, G cursore sempre centrato
 map("n", "<C-d>", "<C-d>zz", { noremap = true })
 map("n", "<C-u>", "<C-u>zz", { noremap = true })
 map("n", "<s-g>", "<s-g>zz", { noremap = true })
 
--- Vimtex
-map("n", "<leader>ll", "<cmd>:VimtexCompile<cr>")
-
--- Open Lazy
-map("n", "<leader>l", "<cmd>:Lazy<cr>")
+-- CTRL+S al posto di s (colpa di leap.nvim)
+map("n", "<C-s>", "s", { noremap = true })
 
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>")
-
--- Telescope
-map("n", "<leader>ff", "<cmd>:Telescope find_files<cr>")
 
 -- Fixa l'ultimo errore
 map("i", "<C-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { noremap = true, silent = true })
@@ -44,8 +40,8 @@ map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>")
 map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>")
 map("n", "<C-h>", "<cmd>BufferLineMovePrev<cr>")
 map("n", "<C-l>", "<cmd>BufferLineMoveNext<cr>")
-
--- ToggleTerm
-map("n", "<C-o>", "<cmd>:ToggleTerm<cr>")
-map("t", "<C-o>", [[<C-\><C-n>]])
-map("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+map("n", "<C-p>", "<cmd>BufferLinePick<cr>")
+map("n", "<leader>cp", "<cmd>BufferLinePickClose<cr>")
+map("n", "<leader>cl", "<cmd>BufferLineCloseRight<cr>")
+map("n", "<leader>ch", "<cmd>BufferLineCloseLeft<cr>")
+map("n", "<leader>co", "<cmd>BufferLineCloseOthers<cr>")
